b11aa2085bc1ba818af777526fa244960b6c6872
tzacher <thomas.zacher@hof-university.de>
2026-04-17 09:13:50 +0200

    8

--- travelsapp/Exercise8/database/seeders/UsersSeeder.php
+++ travelsapp/Exercise8/database/seeders/UsersSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Mapping\UsersTable_V1 as Table;
+use App\Models\Mapping\UsersTable_V2 as Table;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -16,6 +16,7 @@ class UsersSeeder extends Seeder
             Table::COLUMN_NAME => 'root',
             Table::COLUMN_EMAIL => 'root@hof-university.de',
             Table::COLUMN_PASSWORD => Hash::make('test1234'),
+            Table::COLUMN_IS_ADMIN => true
         ]);
         DB::table(Table::TABLE_NAME)->insert([
             Table::COLUMN_NAME => 'antragsteller',
@@ -26,11 +27,13 @@ class UsersSeeder extends Seeder
             Table::COLUMN_NAME => 'mitzeichner',
             Table::COLUMN_EMAIL => 'mitzeichner@hof-university.de',
             Table::COLUMN_PASSWORD => Hash::make('test1234'),
+            Table::COLUMN_IS_VERIFIER => true
         ]);
         DB::table(Table::TABLE_NAME)->insert([
             Table::COLUMN_NAME => 'genehmiger',
             Table::COLUMN_EMAIL => 'genehmiger@hof-university.de',
             Table::COLUMN_PASSWORD => Hash::make('test1234'),
+            Table::COLUMN_IS_APPROVER => true
         ]);
 
         // Additional users
