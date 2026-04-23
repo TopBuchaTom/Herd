c8eaf2217f0067a7e5997c5267401bc6a06026f8
tzacher <thomas.zacher@hof-university.de>
2026-04-23 08:51:05 +0200

    16

--- travelsapp/Exercise16/routes/web.php
+++ travelsapp/Exercise16/routes/web.php
@@ -21,5 +21,6 @@ Route::middleware('auth')->group(function () {
 
 Route::resource('/users', UserController::class)->middleware(['auth']);
 Route::resource('/travels', TravelController::class)->middleware(['auth']);
+Route::post('/travels/create', [TravelController::class, 'create'])->middleware(['auth']);
 
 require __DIR__.'/auth.php';
